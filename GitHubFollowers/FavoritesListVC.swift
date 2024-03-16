64c49d8d5c0db313431636f8a5ee837567be3232
Thierno Diallo <tdiallo1996@gmail.com>
2024-03-16 19:53:51 -0400

    Basic Setup

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesListVCViewController.swift
+//  GitHubFollowers
+//
+//  Created by Thierno Diallo on 3/16/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Thierno Diallo on 3/16/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
