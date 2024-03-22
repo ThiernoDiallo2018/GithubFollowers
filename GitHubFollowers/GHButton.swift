032e8ea59b8734f3fd37396788b47fac2c361bee
Thierno Diallo <tdiallo1996@gmail.com>
2024-03-21 23:50:19 -0400

    Sign In Screen

---
+++ GitHubFollowers/GHButton.swift
@@ -0,0 +1,39 @@
+//
+//  GHButton.swift
+//  GitHubFollowers
+//
+//  Created by Thierno Diallo on 3/21/24.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        
+        // Setting the GHButton Class Background Color to the Background Color we pass through the init. Same for title.
+        
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
